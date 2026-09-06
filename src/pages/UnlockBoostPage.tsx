730d2fd2c82cb52dd9b1063843778d4f002d5af4
jess SEWOVON <jesssewovon@gmail.com>
2026-09-06 22:29:03 +0000

    Updates

--- src/pages/UnlockBoostPage.tsx
+++ src/pages/UnlockBoostPage.tsx
@@ -205,14 +205,14 @@ export default function UnlockBoostPage() {
                   seconds: '',
                 })}
                 <Countdown remainingMs={remainingMs} onEnd={reload} />
-                <button
+                {/* <button
                   type="button"
                   onClick={reload}
                   className="ml-2 inline-flex align-middle text-primary"
                   aria-label={t('reload', { defaultValue: 'reload' })}
                 >
                   <RotateCcw size={16} />
-                </button>
+                </button> */}
               </div>
             )}
 
